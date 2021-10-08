c4d47da4a1642d8babca626c8361642a5948ed7d
oleh10hoba <oleg.10@ukr.net>
2021-10-08 15:29:22 +0200

    OH add favotite icon

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Card, Image, Icon, Button } from 'semantic-ui-react';
-
-
+// import { faHeart } from "@fortawesome/free-solid-svg-icons";
+// import { faHeartBroken } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const ProductCard = ( product) => {
     const {  name, description, image, price, addToCart, addedCount, addToFavourites, isFavorite } = product;
     return(
     <Card>
+      
         <Image src={image}/>
         <Card.Content>
         <Card.Header>{name}</Card.Header>
@@ -18,6 +20,18 @@ const ProductCard = ( product) => {
         <p>
           <Icon name="money" />
           {price}
+          <div className="Like">
+            <button
+              onClick={addToFavourites.bind(this, product)}
+            >
+                {isFavorite 
+                ?
+                  <i className="material-icons">favorite</i>
+                : 
+                <i className="material-icons">favorite_border</i>
+                }
+            </button>
+          </div>
         </p>
       </Card.Content>
       <Button 
@@ -26,15 +40,18 @@ const ProductCard = ( product) => {
         Dodaj do kosza
         {addedCount > 0  && `(${addedCount})`}
     </Button>
-        <Button
-            onClick={addToFavourites.bind(this, product)}
-        >
-            {isFavorite 
-            ?
-            <h4>Usuń z   ulubionych</h4>
-            : 
-            <h4>Dodaj do ulubionych</h4>}
-        </Button>
+        
+        {/* <div
+            className="container"
+            style={{ border: "1px solid black", width: "15%" }}
+            onClick={() => this.toggle()}
+          >
+            {this.state.liked === false ? (
+              <FontAwesomeIcon icon={faHeart} />
+            ) : (
+              <FontAwesomeIcon icon={faHeartBroken} />
+            )}
+          </div> */}
     </Card>
     );
 };
