1c20b6a872be259b5e368a70639d549ccd1e7cfa
Oleh Foliush <olvbman@gmail.com>
2021-11-20 17:51:45 +0200

    OF MINOR FIXES

--- client/src/utils/validators/formcontrols.js
+++ client/src/utils/validators/formcontrols.js
@@ -3,9 +3,9 @@ import styles from "./formcontrols.module.css"
 export const Input = ({input, meta, ...props}) => {
     const Error = meta.touched && meta.error
     return (
-        <div >
         <div>
-            <input {...input} {...props} className={styles.formControl + " " + (Error ? styles.error : "")} />
+        <div>
+            <input {...input} {...props}  />
         </div>
         </div>
     )
